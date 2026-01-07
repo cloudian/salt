b76d3ab09839e79bb156b238a530e401530c44d7
dwojtasiewicz-cloudian <dwojtasiewicz@cloudian.com>
2026-01-07 15:24:48 +0000

    (CI) - ctrl+s

--- tools/utils/__init__.py
+++ tools/utils/__init__.py
@@ -263,7 +263,7 @@ class Version(packaging.version.Version):
 
 
 def get_salt_releases(
-    ctx: Context, repository: str = "cloudian/salt"
+    ctx: Context, repository: str = "saltstack/salt"
 ) -> list[Version]:
     """
     Return a list of salt versions
